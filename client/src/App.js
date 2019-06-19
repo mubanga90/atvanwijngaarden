1a7fd7e8efe68264f41856542d2252c730e4223d
Mubanga <douwemdevries@gmail.com>
2019-06-19 15:07:20 +0200

    update na campaign

--- client/src/App.js
+++ client/src/App.js
@@ -17,8 +17,8 @@ class Nav extends Component {
 
 					<div className="menu">
 						<AnchorLink href="#hero">Home</AnchorLink>
-						<AnchorLink href="#wie">Over</AnchorLink>
-						<AnchorLink href="#onderzoek">Onderzoek</AnchorLink>
+						<AnchorLink href="#auteur">Auteur</AnchorLink>
+						<AnchorLink href="#bestellen">Bestellen</AnchorLink>
 						<AnchorLink href="#sponsoren">Sponsoren</AnchorLink>
 						<AnchorLink href="#steun">Steun</AnchorLink>
 						<AnchorLink href="#contact">Contact</AnchorLink>
@@ -51,8 +51,8 @@ class Section extends Component {
 		if (this.props.subs && this.props.subs.length > 0) {
 			console.log(this.props.subs)
 			this.props.subs.forEach (function(el, i) {
-				subs.push(<Sub 
-					id 		= {i} 
+				subs.push(<Sub
+					id 		= {i}
 					title 	= {el[0]}
 					text 	= {el[1]}
 					/>
@@ -78,58 +78,64 @@ class App extends Component {
 		return (
 			<div className="App">
 				<Nav />
-					
+
 
 				<div className="container">
 					<div id="hero">
 						<img src={logo} alt="AT van Wijngaarden logo" id="logo" />
+						<h5>Op 29 juni 2019 verschijnt het boek:</h5>
 						<h4>De architect A.T. van Wijngaarden (1819-1900)</h4>
-<h5>Een eclectische signatuur in het West-Friese landschap</h5>
+						<h5>Een eclectische signatuur in het West-Friese landschap</h5>
 					</div>
 
-					<Section 
+					<Section
 						id    = "introductie"
-						title = ""
-						text  = "Veel beeldbepalende en vaak monumentale panden in Medemblik en tientallen West-Friese dorpen zijn van de hand van één architect: Arnoldus Teunis van Wijngaarden. De architect van deze monumentale panden heeft tot nu toe te weinig aandacht gekregen. Daarin brengen we verandering met de uitgave van een boek over zijn leven en zijn indrukwekkende bouwoeuvre. Het boek over zijn leven en werk verschijnt ter gelegenheid van zijn 200ste geboortedag, op 29 juni 2019."
+						title = "Over het boek"
+						text  = "Veel beeldbepalende en vaak monumentale panden in Medemblik, Oostwoud, Midwoud, Nieuwe Niedorp, Winkel, Abbekerk en tientallen andere West-Friese plaatsen zijn van de hand van één architect: Arnoldus Teunis van Wijngaarden (1819-1900). Zijn oeuvre omvat zeker 57 gebouwen – kerken, raadhuizen, pastorieën, scholen, wees- en armenhuizen en postkantoren – waarvan er 41 nog tot op de dag van vandaag te bewonderen zijn. Maar liefst zeven ervan hebben de status van rijksmonument.<br><br>
+							Toch is A.T. van Wijngaarden, die ook nog eens meer dan 50 jaar stadsarchitect van Medemblik was, tot nu toe vrijwel onbekend gebleven. Onterecht, want met zijn eclectische stijl, vergelijkbaar met die van zijn beroemde tijdgenoot Pierre Cuypers, drukte hij een heel eigen stempel op het West-Friesland van zijn tijd.<br><br>
+							Dit boek voert u langs leven en werk van A.T. van Wijngaarden, met beschrijvingen van de totstandkoming en de architectuurkenmerken van alle 57 panden, inclusief uniek beeldmateriaal."
 					/>
 
-					<Section 
-						id    = "wie"
-						title = "Wie is A.T. van Wijngaarden?"
-						text  = "Op 29 juni 1819 wordt Arnoldus Teunis in Papendrecht geboren. Al op 26-jarige leeftijd wordt hij benoemd tot ‘Stads Architect’ van Medemblik. Niet lang daarna komen de eerste verzoeken binnen uit omliggende dorpen en steden om kerken en pastorieën, gemeentehuizen, scholen en onderwijzerswoningen, postkantoren en wees- en armenhuizen te ontwerpen. In de loop van een halve eeuw resulteert dat in tientallen karakteristieke panden, vaak in eclectische stijl, die in deze dorpen en steden nog steeds een bijzondere plaats innemen. De cultuur-historische waarde ervan wordt onderstreept door de status van rijks- of provinciaal monument van een aantal gebouwen. <br> <a href='https://nl.wikipedia.org/wiki/Arnoldus_Teunis_van_Wijngaarden'>wikipedia</a>"
+					<Section
+						id    = "auteur"
+						title = "Over de auteur"
+						text  = "A.T. van Wijngaarden is de betovergrootvader van Florence van Wijngaarden, de echtgenote van auteur Gerard de Vries. Geïntrigeerd door het ontbreken van informatie over deze voorouder begint De Vries, in het dagelijks leven arts-epidemioloog en een leek in de architectuur(geschiedenis), een eigen zoektocht die al snel prachtige vondsten oplevert. In dit boek ontsluit hij het indrukwekkende bouwoeuvre van de Medemblikse ‘StadsArchitect’ voor alle liefhebbers van architectuur, van geschiedenis of gewoon van het West-Friese landschap."
 					/>
 
-					<Section 
-						id    = "onderzoek"
-						title = "Succesvol onderzoek"
-						text  = "Meer dan anderhalf jaar onderzoek heeft een rijkdom aan gegevens aan het licht gebracht. In regionale archieven duikt de naam A.T. van Wijngaarden vele honderden keren op: in notulen van gemeenteraads- of kerkraadsvergaderingen als hij bouwplannen komt toelichten, in handgeschreven briefjes aan burgemeesters over de voortgang van de bouw, in nota’s en uiteraard in bestekken, begrotingen en tekeningen van de gebouwen. De archiefbronnen geven een goed beeld van zijn bouwactiviteiten en zijn persoonlijkheid. <br> <br>
-Andere belangrijke bronnen zijn de huidige bezitters/bewoners van de panden, de historische verenigingen en andere familieleden van A.T. van Wijngaarden.<br><br>
-<a href='/Artikel.jpg' target='_blank'>Artikel Noord-Hollands Dagblad van 17 augustus 2018</a> <br> <br>
-<a href='/Brochure project-boek Architect AT van Wijngaarden.pdf' class='button' target='_blank'>Brochure</a>
+					<Section
+						id    = "bestellen"
+						title = "Hoe te bestellen?"
+						text  = "Het boek kost 25 euro (exclusief verzendkosten) en is te bestellen via <a href='mailto:info@atvanwijngaarden.nl'>info@atvanwijngaarden.nl</a> . Het boek is vanaf 1 juli 2019 ook verkrijgbaar bij het Kasteel Radboud te Medemblik. Met enkele boekwinkels in de West-Friese regio is overleg om het boek in deze boekwinkels te verkopen. In de eerste twee weken van juli zal informatie daarover worden aangepast op deze website.<br><br>
+
+							Het boek werd financieel ondersteund door de gemeente Medemblik, Stichting Stadsherstel Medemblik, de gemeente Hollands Kroon en door verschillende anderen via crowdfunding.
 "
 					/>
 
-					<Section 
-						id    = "sponsoren"
-						title = "Voorintekenen en sponsoren"
-						text  = "Een boek over architect A.T. van Wijngaarden vraagt om professionele vormgeving en drukwerk. De totale productiekosten van deze gebonden uitgave, van 120 pagina’s met veel beeldmateriaal en met een harde omslag, hebben we beperkt weten te houden tot circa € 12.500,-. We willen het boek betaalbaar houden en voor € 25,- verkopen. Bij een geschatte verkoop van 300 exemplaren, levert dit een restbedrag op van € 5.000,-. Dit bedrag willen we via sponsoring door organisaties en crowdfunding aanvullen.
+					<Section
+						id    = "verder"
+						title = "En Verder"
+						text  = "<ul>
+							<li>Van juni-september 2019 is de expositie ‘De gebouwen van A.T. van Wijngaarden door de ogen van de kunstenaars’ in de ontvangsthal van het gemeentehuis te Wognum (middengebouw). De expositie laat werken zien van zes kunstenaars (schilderijen, etsen, een pentekening en foto’s).</li>
+							<li>Op <a href='https://nl.wikipedia.org/wiki/Arnoldus_Teunis_van_Wijngaarden' target='_blank'>Wikipedia</a> is een pagina over A.T. van Wijngaarden aangemaakt met zijn bouwoeuvre</li>
+							<li>In 2018 verscheen een <a href='/Artikel.jpg' target='_blank'>artikel</a> over het project en de architect in het Noord-Hollands Dagblad.</li>
+						</ul>
 						"
 					/>
 
-					<Section 
+					<Section
 						id    = "steun"
 						title = "Steun dit project"
 						text  = "<ol><li>Door in te tekenen voor één of meerdere exemplaren van het boek. Als vroeg-inschrijver betaalt u € 20,-.</li>
 <li>Sponsoren van het project en het boek is ook mogelijk. Elk bedrag is welkom. Als u dat wilt komt uw naam in het boek te staan. Voor organisaties is naamsvermelding met logo mogelijk vanaf € 500,-.</li>
 </ol>
 
-U kunt zich inschrijven als vroeg-inschrijver of sponsor door een ingevuld formulier terug te sturen naar: 
-	<a href='mailto: info@atvanwijngaarden.nl'> info@atvanwijngaarden.nl</a> <br> <br> 
+U kunt zich inschrijven als vroeg-inschrijver of sponsor door een ingevuld formulier terug te sturen naar:
+	<a href='mailto: info@atvanwijngaarden.nl'> info@atvanwijngaarden.nl</a> <br> <br>
 <a href='/Formulier Voorinschrijven.docx' class='button' target='_blank'>Formulier vroeg-inschrijver</a>
 <a href='/Formulier Crowdfunding en sponsoring.docx' class='button' target='_blank'>Formulier Crowdfunding & Sponsoren</a>"
 					/>
 
-					<Section 
+					<Section
 						id    = "contact"
 						title = "contact"
 						text  = "Heeft u verdere vragen of opmerkingen?<br> mail dan met:<br> <br>
